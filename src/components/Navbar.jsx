74e263172c5c15bd8e6c02ee454735e8c30be7cf
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-09-13 19:26:38 +0600

    creat login hero form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,11 @@ const Navbar = () => {
 		<>
 			<li>
 				<NavLink to="/">Home</NavLink>
+			</li>
+			<li>
 				<NavLink to="/login">Login</NavLink>
+			</li>
+			<li>
 				<NavLink to="/register">Register</NavLink>
 			</li>
 		</>
